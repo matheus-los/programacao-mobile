ecf19eda79e8568a33deaeb9359adb681b442234
Matheus Lós <158214554+matheus-los@users.noreply.github.com>
2025-04-21 19:17:03 -0300

    Update prototipo5.dart

--- prova01/prototipo5.dart
+++ prova01/prototipo5.dart
@@ -1,4 +1,4 @@
-//Cópia do Instragram
+//Cópia do Instagram
 
 import 'package:flutter/material.dart';
 
@@ -14,7 +14,13 @@ class InstagramClone extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('InstaClone'),
+          title: Row(
+            children: [
+              Icon(Icons.camera_alt, color: Colors.black),
+              SizedBox(width: 8),
+              Text('InstaClone'),
+            ],
+          ),
           backgroundColor: Colors.white,
           titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
         ),
@@ -22,7 +28,7 @@ class InstagramClone extends StatelessWidget {
         body: Column(
           children: [
             Container(
-              height: 100,
+              height: 120,
               padding: EdgeInsets.symmetric(vertical: 8),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
@@ -32,10 +38,21 @@ class InstagramClone extends StatelessWidget {
                       padding: EdgeInsets.symmetric(horizontal: 8),
                       child: Column(
                         children: [
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundImage: NetworkImage(
-                                'https://randomuser.me/api/portraits/men/$index.jpg'),
+                          Container(
+                            padding: EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              gradient: LinearGradient(
+                                colors: [Colors.red, Colors.orange],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                            ),
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: NetworkImage(
+                                  'https://randomuser.me/api/portraits/men/$index.jpg'),
+                            ),
                           ),
                           SizedBox(height: 4),
                           Text('User $index', style: TextStyle(fontSize: 12)),
@@ -50,7 +67,7 @@ class InstagramClone extends StatelessWidget {
               child: ListView.builder(
                 itemCount: 5,
                 itemBuilder: (context, index) {
-                  return PostWidget(userName: 'User $index', postIndex: index,);
+                  return PostWidget(userName: 'User $index', postIndex: index);
                 },
               ),
             ),
@@ -65,30 +82,51 @@ class PostWidget extends StatelessWidget {
   final String userName;
   final int postIndex;
 
-  const PostWidget({Key? key, required this.userName, required this.postIndex}) : super(key: key);
+  const PostWidget({Key? key, required this.userName, required this.postIndex})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 16),
+      margin: EdgeInsets.only(bottom: 16, left: 8, right: 8),
       padding: EdgeInsets.all(12),
       decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            // ignore: deprecated_member_use
+            color: Colors.black.withOpacity(0.05).withValues(),
+            blurRadius: 5,
+            offset: Offset(0, 4),
+          ),
+        ],
         border: Border(
           bottom: BorderSide(color: Colors.grey.shade300),
         ),
+        borderRadius: BorderRadius.circular(12),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(userName, style: TextStyle(fontWeight: FontWeight.bold)),
+              CircleAvatar(
+                radius: 16,
+                backgroundImage: NetworkImage(
+                    'https://randomuser.me/api/portraits/men/$postIndex.jpg'),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(userName,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
               ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blue,
                   foregroundColor: Colors.white,
+                  shape: StadiumBorder(),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 ),
                 child: Text('Seguir'),
               ),
@@ -104,7 +142,7 @@ class PostWidget extends StatelessWidget {
                   color: Colors.grey[300],
                   image: DecorationImage(
                     image: NetworkImage(
-                        'https://picsum.photos/seed/post$postIndex/600/300', scale: 1.0),
+                        'https://picsum.photos/seed/post$postIndex/600/300'),
                     fit: BoxFit.cover,
                   ),
                   borderRadius: BorderRadius.circular(12),
